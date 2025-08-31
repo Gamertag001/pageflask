dbfc5c480ca53d2c38350c76b4076683677e2b6b
Gamertag <gamertag2034@gmail.com>
2025-08-30 21:04:30 -0500

    Update suma.py

--- suma.py
+++ suma.py
@@ -4,16 +4,16 @@ suma = Flask(__name__)
 
 @suma.route('/')
 def inicio():
-	return render_template('inicio2.html')
+    return render_template('inicio2.html')
 
 @suma.route('/calculadora', methods=['GET', 'POST'])
 def formulario():
-	if request.method == 'POST':
-		numero_1 = int(request.form['numero 1'])
-		numero_2 = int(request.form['numero 2'])
-		total = request.form['total']
-		return render_template('response_suma.html',numero_1=numero_1, numero_2=numero_2, total=total)
-	return render_template('form2.html')
+    if request.method == 'POST':
+        numero_1 = int(request.form['numero_1'])
+        numero_2 = int(request.form['numero_2'])
+        total = numero_1 + numero_2
+        return render_template('response_suma.html', numero_1=numero_1, numero_2=numero_2, total=total)
+    return render_template('form2.html')
 
 if __name__ =='__main__':
-	suma.run(debug=True)
+    suma.run(debug=True)
